3d46c5df0d82b16d016d890f51ecca8276e3a5c1
Meghan Hartman <meghan.hartman@nyu.edu>
2015-02-17 18:06:19 +0000

    change actionmailer configuration

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Configure ActionMailer
+ActionMailer::Base.smtp_settings = {
+    :user_name => 'app34086970@heroku.com',
+    :password => 'rfdmgyac',
+    :domain => 'goverlan-order-summaries.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+}
--- config/initializers/mailer_settings.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-    :user_name => 'app34086970@heroku.com',
-    :password => 'rfdmgyac',
-    :domain => 'goverlan-order-summaries.herokuapp.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-}
