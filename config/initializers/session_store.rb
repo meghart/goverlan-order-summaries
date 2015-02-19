d51b524ab004c6a9a26d1b27147580eb9e18ef46
Meghan Hartman <meghan.hartman@nyu.edu>
2015-02-19 23:59:06 +0000

    bug cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,9 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, { key: '_workspace_session' },
-                                        :iron_cache, {
+Rails.application.config.session_store :iron_cache,
                                           project_id: '54e671cdc4662500060000b1',
                                           token: '5ffLAHf2WBrrOm3UnGth8SC6P8M',
                                           namespace: 'goverlan-order-summaries [IronCache]',
                                           expires_in: 7200
-                                        }
+                                        
