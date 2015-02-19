7a7ed4c285c86ce58840efca5ae5c86ef93bac89
Meghan Hartman <meghan.hartman@nyu.edu>
2015-02-19 18:17:02 +0000

    change application.rb file to match saleforce example

--- config/application.rb
+++ config/application.rb
@@ -10,12 +10,74 @@ Bundler.require(*Rails.groups)
 
 module Workspace
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+     configure do
+      enable :logging
+      enable :sessions
+      set :show_exceptions, false
+      set :session_secret, ENV['SECRET']
+    end
+  
     use OmniAuth::Builder do
       provider :salesforce, ENV['SALESFORCE_KEY'], ENV['SALESFORCE_SECRET']
     end
+  
+    before /^(?!\/(auth.*))/ do   
+      redirect '/authenticate' unless session[:instance_url]
+    end
+  
+  
+    helpers do
+      def client
+        @client ||= Force.new instance_url:  session['instance_url'], 
+                              oauth_token:   session['token'],
+                              refresh_token: session['refresh_token'],
+                              client_id:     ENV['SALESFORCE_KEY'],
+                              client_secret: ENV['SALESFORCE_SECRET']
+      end
+  
+    end
+  
+  
+    get '/' do
+      logger.info "Visited home page"
+      @accounts= client.query("select Id, Name from Account")    
+      erb :index
+    end
+  
+  
+    get '/authenticate' do
+      redirect "/auth/salesforce"
+    end
+  
+  
+    get '/auth/salesforce/callback' do
+      logger.info "#{env["omniauth.auth"]["extra"]["display_name"]} just authenticated"
+      credentials = env["omniauth.auth"]["credentials"]
+      session['token'] = credentials["token"]
+      session['refresh_token'] = credentials["refresh_token"]
+      session['instance_url'] = credentials["instance_url"]
+      redirect '/'
+    end
+  
+    get '/auth/failure' do
+      params[:message]
+    end
+  
+    get '/unauthenticate' do
+      session.clear 
+      'Goodbye - you are now logged out'
+    end
+  
+    error Force::UnauthorizedError do
+      redirect "/auth/salesforce"
+    end
+  
+    error do
+      "There was an error.  Perhaps you need to re-authenticate to /authenticate ?  Here are the details: " + env['sinatra.error'].name
+    end
+  
+    run! if app_file == $0
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Eastern Time (US & Canada)'
